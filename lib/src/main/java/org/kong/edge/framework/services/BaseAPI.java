17ec1bd4b6ac28539db91a5165cbb287f0ee5795
suryarekha <surya.rekha@electrolux.com>
2024-12-06 10:52:10 +0100

    Improved logging for token

--- lib/src/main/java/org/kong/edge/framework/services/BaseAPI.java
+++ lib/src/main/java/org/kong/edge/framework/services/BaseAPI.java
@@ -1,5 +1,7 @@
 package org.kong.edge.framework.services;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.kong.edge.framework.http.HttpServiceClient;
 import org.kong.edge.framework.utils.JSONUtils;
 import org.kong.edge.framework.utils.TestDataInitializer;
@@ -8,7 +10,8 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public class BaseAPI {
-	
+	private static final Logger log = LogManager.getLogger(BaseAPI.class);
+
 	protected HttpServiceClient client;
 	protected Gson jsonObject;
 	protected JsonObject testData;
@@ -24,7 +27,7 @@ public class BaseAPI {
 		testData = TestDataInitializer.loadTestData();
 		baseUrl = testData.get("baseUrl").getAsString();
 		bearerToken = System.getenv("KONG_API_KEY");
-		System.out.println("Bearer Token Found: "+bearerToken.startsWith("kpat"));
+        log.info("Bearer Token Found: {}", bearerToken.startsWith("kpat"));
 		baseUrl_global = testData.get("baseUrl_global").getAsString();
 	}
 
